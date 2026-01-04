17852d299738268a18906f85c2a5349ace341657
ShinoChan0 <main@shino.zip>
2026-01-04 23:46:44 +0900

    new Date().getFullYear()

--- frontend/src/utils/Footer.js
+++ frontend/src/utils/Footer.js
@@ -55,7 +55,7 @@ const Footer = () => {
                 </div>
                 <div className="flex justify-between max-lg:items-center max-lg:flex-col-reverse px-16 max-md:px-4 py-4 text-sm text-gray-700">
                     <div>
-                        ©️2026 GMCRP
+                        ©️{new Date().getFullYear()} GMCRP
                     </div>
                 </div>
             </footer>
