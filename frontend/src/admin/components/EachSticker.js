7288f4daa620b80c5096cbf7a443c4171e8af0b0
ShinoChan0 <main@shino.zip>
2026-01-06 00:38:01 +0900

    Update EachSticker.js

--- frontend/src/admin/components/EachSticker.js
+++ frontend/src/admin/components/EachSticker.js
@@ -51,12 +51,12 @@ const EachSticker = ({ sticker, token }) => {
                 <div className="w-72 bg-white rounded-lg shadow-md m-4 relative pt-2 pb-1 flex flex-col justify-between border-accent border-4 hover:bg-gray-300 cursor-pointer hover:border-purple-500">
                     <div className="flex flex-col items-center">
                         {isAdd === 1 &&
-                            <div className="text-end w-full h-9 px-2 text-green-400 font-bold">
+                            <div className="text-end w-full h-72 px-2 text-green-400 font-bold">
                                 追加済み
                             </div>
                         }
                         {isCancel === 1 &&
-                            <div className="text-end w-full h-9 px-2 text-red-600 font-bold">
+                            <div className="text-end w-full h-72 px-2 text-red-600 font-bold">
                                 キャンセル済み
                             </div>
                         }
